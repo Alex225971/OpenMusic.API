b0417fdd05a26021a78901b4461fd2d361f9efeb
Alex225971 <ahaines964@gmail.com>
2024-09-04 11:17:59 +0100

    Added artist ID to album dto for front-end navigation

--- OpenMusic.API/Models/Album/AlbumReadOnlyDto.cs
+++ OpenMusic.API/Models/Album/AlbumReadOnlyDto.cs
@@ -9,6 +9,7 @@ namespace OpenMusic.API.Models.Album
         public string? Genre { get; set; }
         public string Image { get; set; }
         public string ArtistName { get; set; }
+        public int ArtistId { get; set; }
         public List<SongInAlbumDto>? Songs { get; set; }
     }
 }
