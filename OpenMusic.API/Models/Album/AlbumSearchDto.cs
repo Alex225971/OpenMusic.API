cc69fbf3f4a23630ee59c61ef61fb823f0258521
Alex225971 <ahaines964@gmail.com>
2024-10-07 11:02:44 +0100

    Optimised search

---
+++ OpenMusic.API/Models/Album/AlbumSearchDto.cs
@@ -0,0 +1,16 @@
+﻿using OpenMusic.API.Data;
+using OpenMusic.API.Models.Genre;
+using OpenMusic.API.Models.Song;
+
+namespace OpenMusic.API.Models.Album
+{
+    public class AlbumSearchDto : BaseDto
+    {
+        public string Title { get; set; }
+        public int? ArtistId { get; set; }
+        public string ArtistName { get; set; }
+        public string Image { get; set; }
+        public List<SongSearchDto> Songs { get; set; }
+
+    }
+}
